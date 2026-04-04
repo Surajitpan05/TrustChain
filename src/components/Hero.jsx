beb4bbc47b8491c0f1fdfac9e260d1f578743d34
Mohit Thakur <thakurmohit.0102@gmail.com>
2026-04-04 16:04:11 +0530

    Hero initials

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import { S } from "../styles/index.js";
+
+const STATS = [
+  { num: "2.4M+", label: "Products Verified" },
+  { num: "0.3ms", label: "Avg Scan Time" },
+  { num: "100%",  label: "Tamper-Proof" },
+];
