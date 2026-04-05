aeb04dcbafd465df818d290820f591167cbc4cd8
Prasenjit10 <prasenjitsasmal1010@gmail.com>
2026-04-05 09:37:41 +0530

    update

--- src/components/ProductDashboard.jsx
+++ src/components/ProductDashboard.jsx
@@ -794,7 +794,7 @@ setProduct({
   mfd: product.mfd,
   expiry: product.expiry,
   mrp: product.mrp,
-  riskScore: backendRisk
+  riskScore: int(backendRisk)
 });
       // ✅ Fetch ONLY risk from backend
 
